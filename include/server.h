5381dee6dc8ac3b420e2eef3583d770fac4ec3c3
middleprince <hellangel.ah@gmail.com>
2019-08-22 22:37:57 +0800

    feat: Add server header

--- include/server.h
+++ include/server.h
@@ -19,10 +19,39 @@ public:
     Server();
     ~Server();
 
+    bool startServer();
+    bool init(const std::string &conf_path);
+    bool uninit();
     
+    void askToQuit();
 private:
-    
+    bool _listenAtPort(); 
+    bool _closeConnection(int fd);
 
+    bool _handleEvent(const epoll_event &ev);
+    bool _handleAccept();
+    bool _handleRead(const epoll_event &ev);
+    bool _handleWrite(cosnt epoll_event &ev);
+
+    Configure _conf; // configure tool class
+    int _listenfd;  // file descriptor
+    int _epollfd; // epoll descriptor
+    std::map<int, Connection*> _connections; // map for every fd and connection 
+    ThreadPool<Connection> _threadpool; // thread pool for http parse
+    bool is_running; //is server running
+
+    // server configurations
+    int _port;
+    int _backlog;
+    int _max_events;
+    int _max_threads;
+    int _max_requests;
+    std::string _root_dir;
+
+    // delete copy and assign
+    Server(const Server &other);
+    Server& operator=(const Server &rhs);
+    
 };
 
 }
